876f4a0339b20a7e43739ca0134d44dfdcc767e7
kakake <343588387@qq.com>
2016-04-24 22:22:15 +0800

    demo

---
+++ his_controlsdemo/ViewForm/FrmCustomControls.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace his_controlsdemo.ViewForm
+{
+    partial class FrmCustomControls
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGrid1 = new EfwControls.CustomControl.DataGrid();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.popup1 = new EfwControls.CustomControl.Popup(this.components);
+            this.书籍名称 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.价格 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.购买时间 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel6);
+            this.groupBox1.Controls.Add(this.panel5);
+            this.groupBox1.Controls.Add(this.panel4);
+            this.groupBox1.Controls.Add(this.panel3);
+            this.groupBox1.Controls.Add(this.panel2);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(5, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(982, 314);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "控件展示效果";
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(420, 255);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(200, 100);
+            this.panel6.TabIndex = 5;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(648, 137);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(200, 100);
+            this.panel5.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(648, 20);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 100);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(420, 137);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(420, 21);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGrid1);
+            this.panel1.Location = new System.Drawing.Point(32, 20);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(341, 213);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGrid1
+            // 
+            this.dataGrid1.AllowSortWhenClickColumnHeader = false;
+            this.dataGrid1.AllowUserToAddRows = false;
+            this.dataGrid1.AllowUserToDeleteRows = false;
+            this.dataGrid1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.AliceBlue;
+            this.dataGrid1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGrid1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(239)))), ((int)(((byte)(255)))));
+            this.dataGrid1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.书籍名称,
+            this.价格,
+            this.购买时间});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGrid1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGrid1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(215)))), ((int)(((byte)(229)))));
+            this.dataGrid1.HighlightSelectedColumnHeaders = false;
+            this.dataGrid1.Location = new System.Drawing.Point(0, 0);
+            this.dataGrid1.Name = "dataGrid1";
+            this.dataGrid1.RowHeadersWidth = 25;
+            this.dataGrid1.RowTemplate.Height = 23;
+            this.dataGrid1.SelectAllSignVisible = false;
+            this.dataGrid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGrid1.SeqVisible = true;
+            this.dataGrid1.Size = new System.Drawing.Size(341, 213);
+            this.dataGrid1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Location = new System.Drawing.Point(5, 319);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(982, 208);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "控件使用代码";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 17);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(976, 188);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // 书籍名称
+            // 
+            this.书籍名称.DataPropertyName = "BookName";
+            this.书籍名称.HeaderText = "书籍名称";
+            this.书籍名称.MinimumWidth = 120;
+            this.书籍名称.Name = "书籍名称";
+            this.书籍名称.ReadOnly = true;
+            this.书籍名称.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.书籍名称.Width = 120;
+            // 
+            // 价格
+            // 
+            this.价格.DataPropertyName = "BuyPrice";
+            this.价格.HeaderText = "价格";
+            this.价格.MinimumWidth = 100;
+            this.价格.Name = "价格";
+            this.价格.ReadOnly = true;
+            this.价格.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // 购买时间
+            // 
+            this.购买时间.DataPropertyName = "BuyDate";
+            this.购买时间.HeaderText = "购买时间";
+            this.购买时间.MinimumWidth = 120;
+            this.购买时间.Name = "购买时间";
+            this.购买时间.ReadOnly = true;
+            this.购买时间.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.购买时间.Width = 120;
+            // 
+            // FrmCustomControls
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 532);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "FrmCustomControls";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Text = "FrmCustomControls";
+            this.OpenWindowBefore += new System.EventHandler(this.FrmCustomControls_OpenWindowBefore);
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private EfwControls.CustomControl.DataGrid dataGrid1;
+        private EfwControls.CustomControl.Popup popup1;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 书籍名称;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 价格;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 购买时间;
+    }
+}
---
+++ his_controlsdemo/ViewForm/FrmCustomControls.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EFWCoreLib.WinformFrame.Controller;
+
+namespace his_controlsdemo.ViewForm
+{
+    public partial class FrmCustomControls : BaseForm
+    {
+        public FrmCustomControls()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmCustomControls_OpenWindowBefore(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            panel2.Visible = false;
+            panel3.Visible = false;
+            panel4.Visible = false;
+            panel5.Visible = false;
+            panel6.Visible = false;
+            switch (frmName)
+            {
+                case "DataGrid":
+                    panel1.Visible = true;
+                    panel1.Dock = DockStyle.Fill;
+
+                    dataGrid1.DataSource=(DataTable)InvokeController("GetBooks");
+                    break;
+                case "GridBoxCard":
+                    panel2.Visible = true;
+                    panel2.Dock = DockStyle.Fill;
+                    break;
+                case "TextboxCard":
+                    panel3.Visible = true;
+                    panel3.Dock = DockStyle.Fill;
+                    break;
+                case "MultiSelectText":
+                    panel4.Visible = true;
+                    panel4.Dock = DockStyle.Fill;
+                    break;
+                case "StatDateTime":
+                    panel5.Visible = true;
+                    panel5.Dock = DockStyle.Fill;
+                    break;
+                case "Popup":
+                    panel6.Visible = true;
+                    panel6.Dock = DockStyle.Fill;
+                    break;
+            }
+        }
+    }
+}
